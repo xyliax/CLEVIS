f2655b2d8e52d84cea854a8239c281d57515533f
pyx-2021 <20083743d@connect.polyu.hk>
2021-11-15 16:58:19 +0800

    update

--- src/hk/edu/polyu/comp/comp2021/clevis/Application.java
+++ src/hk/edu/polyu/comp/comp2021/clevis/Application.java
@@ -1,6 +1,7 @@
 package hk.edu.polyu.comp.comp2021.clevis;
 
 import hk.edu.polyu.comp.comp2021.clevis.model.Clevis;
+import org.w3c.dom.ls.LSOutput;
 
 import java.io.Console;
 import java.util.Scanner;
@@ -20,20 +21,63 @@ public class Application {
 		// FIXME: 10/11/2021
 		Scanner scanner = new Scanner(System.in);
 		while (clevis.isRunning()) {
-			if (scanner.nextLine().strip().equalsIgnoreCase("man")) {
-				displayUserManual();
+			String input = scanner.nextLine();
+			if (input.matches("man .+")) {
+				displayUserManual(input.substring(3).strip().toLowerCase());
 				continue;
 			}
-			clevis.request(scanner.nextLine());
+			clevis.request(input);
 		}
 	}
 
-	private static void displayUserManual() {
-		System.out.println("""
+	private static void displayUserManual(String commandName) {
+		switch (commandName) {
+			case "line" -> System.out.println(Manual.LINE.getText());
+			//...finish all cases
+			default -> System.out.println(Manual.CLEVIS.getText());
+		}
+	}
+
+	/**
+	 *
+	 */
+	enum Manual {
+		/**
+		 * display all
+		 */
+		CLEVIS("""
 				........................CLEVIS-USER-MANUAL........................
 								
-				""");
-	}
+				"""),
+
+		/**
+		 *
+		 */
+		LINE("""
+							........................CLEVIS-USER-MANUAL........................
+				"""),
+
+		//complete other commands
+		private String text;
+
+		Manual(String text) {
+			this.setText(text);
+		}
+
+
+		/**
+		 * Getter of text
+		 *
+		 * @return the user manual
+		 */
+		public String getText() {
+			return text;
+		}
+
+		private void setText(String text) {
+			this.text = text;
+		}
+		}
 }
 
 
